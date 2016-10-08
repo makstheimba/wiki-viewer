29c094b2382277fc6a21bd5b23bce80578afe06e
makstheimba <makstheimba@yandex.ru>
2016-10-08 14:42:19 +0300

    Show More button and some styling

--- assets/wiki_viewer.js
+++ assets/wiki_viewer.js
@@ -1,32 +1,47 @@
-//TODO: 
-//Show more items
+//TODO:
 //Styling at small screens
 
 function response_handle() {
-    var response = JSON.parse(this.responseText);
+    var response = JSON.parse(this.responseText),
+        more_btn = document.getElementById("more_btn");
     console.log(response);
+    if (response.hasOwnProperty("continue")){
+        more_btn.onclick = function () {search(response.continue)};
+        more_btn.style.display = "inline-block";
+    }
+    else {
+        more_btn.style.display = "none";
+    }
+    document.getElementById("totop_btn").style.display = "inline-block";
     response.query.search.forEach(function (entry) {
         var nw = document.getElementById("base-snippet").cloneNode();
         nw.onclick = function () {window.open("https://en.wikipedia.org/wiki/" + entry.title, "_blank"); };
         nw.removeAttribute("id");
         nw.className += " search-entry";
         nw.style.display = "block";
-        nw.innerHTML = "<h3>" + entry.title + "</h3>" + entry.snippet;
-        document.body.appendChild(nw);
+        nw.innerHTML = "<h3>" + entry.title + "</h3>" + entry.snippet;        
+        document.body.insertBefore(nw, document.getElementById("wrap"));
     });
 }
 
 function goRandom() {
     window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
 }
+function toTheTop() {
+    window.scrollTo(0,0);
+}
+
 function clearPage() {
     var search_entry = document.getElementsByClassName("search-entry");
+    document.getElementById("more_btn").style.display = "none";
+    document.getElementById("totop_btn").style.display = "none";
+    
     while (search_entry[0]) {
         search_entry[0].parentNode.removeChild(search_entry[0]);
     }
 }
 function searchTermInvalid(st) {
-    if (st === "") {
+    if (st === "") {        
         clearPage();
         return true;
     } else if (st.length > 30) {
@@ -36,16 +51,18 @@ function searchTermInvalid(st) {
     }
     return false;
 }
-function search() {
+function search(cont = {"continue": "", "sroffset": ""}) {
     var wiki_request = new XMLHttpRequest(),
         search_term = document.getElementById("search_field").value;
     if (searchTermInvalid(search_term)) {
         return;
     }
-    clearPage();
+    if (cont.continue === ""){
+        clearPage();
+    }
     wiki_request.addEventListener("load", response_handle);//sroffset
     wiki_request.open("GET", "https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&format=json&utf8=1&srsearch=" +
-                     search_term + "&srprop=snippet|titlesnippet&continue=&sroffset=");
+                     search_term + "&srprop=snippet|titlesnippet&continue=" + cont.continue + "&sroffset=" + cont.sroffset);
     wiki_request.setRequestHeader("Api-User-Agent", "WikipediaViewer/1.0");
     wiki_request.send();
 }
